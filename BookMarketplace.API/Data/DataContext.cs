645495463437a22d2e718e837e08d54f6162cb10
Bishal Poudel <bishal.poudel@crystalsolution.com.np>
2024-04-06 16:36:50 +0545

    Add project files.

---
+++ BookMarketplace.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookMarketplace.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
