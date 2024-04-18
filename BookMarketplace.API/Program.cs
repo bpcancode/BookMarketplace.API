273cce0363396d3791f17ea292b01426a1ba1ff2
Bishal Poudel <bishal.poudel@crystalsolution.com.np>
2024-04-18 21:02:28 +0545

    Backend finished

--- BookMarketplace.API/Program.cs
+++ BookMarketplace.API/Program.cs
@@ -5,20 +5,30 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin();
+                      });
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => 
+builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}) .AddJwtBearer(jwtOptions =>
+}).AddJwtBearer(jwtOptions =>
     jwtOptions.TokenValidationParameters = TokenService.GetTokenValidationParameter(builder.Configuration));
 
 builder.Services.AddAuthorization();
@@ -31,18 +41,15 @@ builder.Services.AddTransient<TokenService>()
 
 var app = builder.Build();
 
-#if DEBUG
-// MigrateDatabase(app.Services);
-#endif
+
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -50,11 +57,3 @@ app.UseAuthorization();
 app.MapEndpoints();
 
 app.Run();
-
-static void MigrateDatabase(IServiceProvider sp)
-{
-    var scope = sp.CreateScope();
-    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
-    if(dataContext.Database.GetPendingMigrations().Any())
-        dataContext.Database.Migrate();
-}
